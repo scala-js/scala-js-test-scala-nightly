1f2487cdab02ae5a32bb8fb7ec0f3002a4f0ea8b
Sébastien Doeraene <sjrdoeraene@gmail.com>
2019-11-13 22:47:06 +0100

    Initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Testing Scala.js against the nightly builds of Scala
+
+Work-in-progress
---
+++ run.sh
@@ -0,0 +1,15 @@
+BRANCH="$1"
+
+SCALA_VERSION=$(wget -q -O - https://raw.githubusercontent.com/scala/community-builds/$BRANCH/nightly.properties | grep '^nightly=' | cut -d '=' -f2)
+SUFFIX=$(echo $BRANCH | tr '.' '_' | cut -d '_' -f1-2)
+
+echo "Branch: $BRANCH"
+echo "Scala version: $SCALA_VERSION"
+echo "Project suffix: $SUFFIX"
+
+rm -rf scala-js/ && \
+git clone https://github.com/scala-js/scala-js.git && \
+cd scala-js && \
+git checkout master && \
+npm install && \
+sbt 'set resolvers in Global += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/"' ++$SCALA_VERSION testSuite$SUFFIX/test
