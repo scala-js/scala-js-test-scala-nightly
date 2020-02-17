c17be7d361d53caabfa20271810aa55a20e7ce9c
Sébastien Doeraene <sjrdoeraene@gmail.com>
2020-02-17 14:37:33 +0100

    Test the 0.6.x branch of Scala.js as well as master.

--- run.sh
+++ run.sh
@@ -1,15 +1,31 @@
-BRANCH="$1"
+SCALA_BRANCH="$1"
+SCALAJS_BRANCH="$2"
 
-SCALA_VERSION=$(wget -q -O - https://raw.githubusercontent.com/scala/community-builds/$BRANCH/nightly.properties | grep '^nightly=' | cut -d '=' -f2)
-SUFFIX=$(echo $BRANCH | tr '.' '_' | cut -d '_' -f1-2)
+SCALA_VERSION=$(wget -q -O - https://raw.githubusercontent.com/scala/community-builds/$SCALA_BRANCH/nightly.properties | grep '^nightly=' | cut -d '=' -f2)
+SUFFIX=$(echo $SCALA_BRANCH | tr '.' '_' | cut -d '_' -f1-2)
 
-echo "Branch: $BRANCH"
+echo "Scala branch: $SCALA_BRANCH"
 echo "Scala version: $SCALA_VERSION"
 echo "Project suffix: $SUFFIX"
+echo "Scala.js branch: $SCALAJS_BRANCH"
+
+case $SCALAJS_BRANCH in
+  0.6.x)
+    TESTS="testSuite/test testSuiteJVM/test ir/test irJS/test"
+    ;;
+  master)
+    TESTS="testSuite$SUFFIX/test testSuiteJVM$SUFFIX/test ir$SUFFIX/test irJS$SUFFIX/test linker$SUFFIX/test linkerJS$SUFFIX/test"
+    ;;
+  *)
+    echo "Unknown Scala.js branch $SCALAJS_BRANCH"
+    exit 1
+esac
+
+echo "Tests:" $TESTS
 
 rm -rf scala-js/ && \
 git clone https://github.com/scala-js/scala-js.git && \
 cd scala-js && \
-git checkout master && \
+git checkout $SCALAJS_BRANCH && \
 npm install && \
-sbt 'set resolvers in Global += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/"' ++$SCALA_VERSION testSuite$SUFFIX/test testSuiteJVM$SUFFIX/test ir$SUFFIX/test irJS$SUFFIX/test linker$SUFFIX/test linkerJS$SUFFIX/test
+sbt 'set resolvers in Global += "scala-integration" at "https://scala-ci.typesafe.com/artifactory/scala-integration/"' ++$SCALA_VERSION $TESTS
